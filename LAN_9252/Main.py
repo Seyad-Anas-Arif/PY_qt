9b2b8f8546b99a92b207eb6b0b17480d471f7064
Seyad-Anas-Arif <164306079+Seyad-Anas-Arif@users.noreply.github.com>
2024-04-26 15:11:06 +0530

    Update Main.py

--- LAN_9252/Main.py
+++ LAN_9252/Main.py
@@ -1,11 +1,28 @@
 # This Python file uses the following encoding: utf-8
 import sys
+from PyQt5.QtWidgets import QWidget, QApplication
 from PyQt5.QtCore import Qt
-import lanc
+import gpiod  # You need to import gpiod
+from gpiod import Direction, Value  # Import required classes from gpiod module
+from lanc import Etc_Read_Reg, etc_init, etc_scan  # Import functions from lanc module
 from ui_st2 import Ui_Widget
-#from form import *
-#from ui_form import Ui_Pushbuttons
-LINE=2
+
+# Define LINE and other variables
+LINE = 2
+etc_init_ok = False
+chip_id = 0
+etc_command = 0
+etc_command_value = 0.0
+etc_status_reg = 0
+received_setpoint = 0.0
+received_command = 0
+
+ETC_HOME_STATUS = 0
+ETC_POSITIVE_LIMIT = 1
+ETC_NEGATIVE_LIMIT = 2
+ETC_DRIVE_FAULT = 3
+ETC_PCAP_ERROR_BIT = 4
+
 class Widget(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
@@ -13,43 +30,29 @@ class Widget(QWidget):
         self.ui.setupUi(self)
         self.ETC_Startup()
 
-
-        self.stat_pb.clicked.connect(turn_off_led)
-        self.stop_pb.clicked.connect(turn_on_led)
-	
+        self.stat_pb.clicked.connect(self.turn_off_led)
+        self.stop_pb.clicked.connect(self.turn_on_led)
 
         self.gpio_request = gpiod.request_lines(
-                   "/dev/gpiochip0",
-                   consumer="led-control",
-                   config={
-                       LINE: gpiod.LineSettings(
-                           direction=Direction.OUTPUT, output_value=Value.INACTIVE
-                       )
-                   },
-               )
+            "/dev/gpiochip0",
+            consumer="led-control",
+            config={
+                LINE: gpiod.LineSettings(
+                    direction=Direction.OUTPUT, output_value=Value.INACTIVE
+                )
+            },
+        )
 
-        def turn_on_led(self):
-                     if self.gpio_request:
-                         self.gpio_request.set_value(LINE, Value.ACTIVE)
+    def turn_on_led(self):
+        if self.gpio_request:
+            self.gpio_request.set_value(LINE, Value.ACTIVE)
 
-                 def turn_off_led(self):
-                     if self.gpio_request:
-                         self.gpio_request.set_value(LINE, Value.INACTIVE)
-                         
-	def Etc_Startup(self):
-	etc_init_ok = False
-        chip_id = 0
-        etc_command = 0
-        etc_command_value = 0.0
-        etc_status_reg = 0
-        received_setpoint = 0.0
-        received_command = 0
+    def turn_off_led(self):
+        if self.gpio_request:
+            self.gpio_request.set_value(LINE, Value.INACTIVE)
 
-        ETC_HOME_STATUS = 0
-        ETC_POSITIVE_LIMIT = 1
-        ETC_NEGATIVE_LIMIT = 2
-        ETC_DRIVE_FAULT = 3
-        ETC_PCAP_ERROR_BIT = 4
+    def ETC_Startup(self):
+        global etc_init_ok, chip_id, etc_command, etc_command_value, etc_status_reg, received_setpoint, received_command
 
         # Read chip ID and initialize EtherCAT
         chip_id = Etc_Read_Reg(ID_REV, 4)
@@ -57,7 +60,7 @@ class Widget(QWidget):
 
         # EtherCAT Communication
         if etc_init_ok:
-        print("Etercat Initialized")
+            print("Etercat Initialized")
             etc_scan()
         else:
             etc_init_ok = etc_init()
@@ -66,21 +69,17 @@ class Widget(QWidget):
         # EtherCAT Command Receive
         received_command = Etc_Buffer_Out.LANLong[0]
         received_setpoint = float(Etc_Buffer_Out.LANFloat[1])
-      
 
         # EtherCAT Send Status
-        etc_status_reg &= ~(1 << ETC_HOME_STATUS)
-        etc_status_reg &= ~(1 << ETC_DRIVE_FAULT)
-        Etc_Buffer_In.LANFloat[2] = float(enc_deg)
-        Etc_Buffer_In.LANFloat[3] = float(enc_rpm)
-        Etc_Buffer_In.LANFloat[4] = float(avg_current)
-        Etc_Buffer_In.LANFloat[0] = float(etc_status_reg)
-
+        #etc_status_reg &= ~(1 << ETC_HOME_STATUS)
+        #etc_status_reg &= ~(1 << ETC_DRIVE_FAULT)
+        Etc_Buffer_In.LANFloat[2] = 18  # enc_deg is not defined in your code
+        Etc_Buffer_In.LANFloat[3] = 1000  # enc_rpm is not defined in your code
+        Etc_Buffer_In.LANFloat[4] = 6 # avg_current is not defined in your code
+        Etc_Buffer_In.LANFloat[0] = 2
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     widget = Widget()
     widget.show()
     sys.exit(app.exec())
-
-
