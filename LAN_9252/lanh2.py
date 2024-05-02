16b947e012cd1a26f3e049e58a48f097ab0f5bcb
Seyad-Anas-Arif <164306079+Seyad-Anas-Arif@users.noreply.github.com>
2024-05-02 12:29:28 +0530

    Update lanh2.py

--- LAN_9252/lanh2.py
+++ LAN_9252/lanh2.py
@@ -157,4 +157,5 @@ class ULONG(ctypes.Union):
 
 class PROCBUFFER(ctypes.Union):
     _fields_ = [("LANByte", ctypes.c_uint8 * 32),
-                ("LANLong", ctypes.c_uint32 * 8)]
+                ("LANFloat", ctypes.c_float * 8),
+	            ("LANLong", ctypes.c_uint32 * 8)]
