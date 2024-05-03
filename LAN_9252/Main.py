83dc461abea75f551db15d56680e7b7a327841e4
Seyad-Anas-Arif <164306079+Seyad-Anas-Arif@users.noreply.github.com>
2024-05-03 14:32:22 +0530

    Update Main.py

--- LAN_9252/Main.py
+++ LAN_9252/Main.py
@@ -4,7 +4,7 @@ from PyQt5.QtWidgets import QWidget, QApplication
 from PyQt5.QtCore import Qt
 import gpiod  # You need to import gpiod
 from gpiod.line import Direction, Value  # Import required classes from gpiod module
-from lanc import Etc_Init,Etc_Scan,Etc_Read_reg# Import functions from lanc module
+from lanc import Etc_Init,Etc_Scan,Etc_Read_Reg# Import functions from lanc module
 from lanh2 import *
 from ui_st2 import Ui_Widget
 
