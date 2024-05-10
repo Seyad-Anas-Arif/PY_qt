e03befc610af8f1fa326c0ca6a4e6e2384c548bf
Seyad-Anas-Arif <164306079+Seyad-Anas-Arif@users.noreply.github.com>
2024-05-10 16:02:04 +0530

    Update uday_spi.py

--- LAN_9252/uday_spi.py
+++ LAN_9252/uday_spi.py
@@ -171,7 +171,7 @@ class UWORD(Union):
 
 class ULONG(Union):
     _fields_ = [("LANLong", c_uint32),                # uint32_t LANLong
-                ("LANWord", c_uint32 * 2),             # uint16_t LANWord[2]
+                ("LANWord", c_uint16 * 2),             # uint16_t LANWord[2]
                 ("LANByte", c_uint8 * 4)]             # uint8_t LANByte[4]
 
 class PROCBUFFER(Union):
@@ -203,7 +203,7 @@ def Etc_Read_Reg(address, length):
     Addr = UWORD()    # Initialize Addr as UWORD instance and set address
     xfrbuf = [0] * 7   # Create buffer for SPI transfer
 
-    Addr.LANWord = address
+    Addr.LANLong = address
 
     xfrbuf[0] = COMM_SPI_READ  # SPI read command
     xfrbuf[1] = Addr.LANByte[1]  # Address high byte
