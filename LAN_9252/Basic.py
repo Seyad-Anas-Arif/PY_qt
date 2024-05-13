5e083f1a3d08969ac405103476eb3349c7371648
Seyad-Anas-Arif <164306079+Seyad-Anas-Arif@users.noreply.github.com>
2024-05-13 11:05:52 +0530

    Update Basic.py

--- LAN_9252/Basic.py
+++ LAN_9252/Basic.py
@@ -1,39 +1,41 @@
 import spidev
+import time
 
 # Initialize SPI
 spi = spidev.SpiDev()
-spi.open(2, 0)  # Open SPI bus 0, device 0
+spi.open(0x002, 0)  # Open SPI bus 0, device 0
 spi.max_speed_hz = 30000000  # Set SPI speed to 30 MHz
 spi.mode = 0b11
 
 # Data to be transmitted
-tx_data = [0x02, 0x01, 0xF8, 0x00, 0x00, 0x00, 0x00]
+# tx_data = [0x02, 0x01, 0xF8, 0x00, 0x00, 0x00, 0x00]
 
 # Perform SPI transaction
-rx_data = spi.xfer2(tx_data)
+# rx_data = spi.xfer2(tx_data)
 
-print(" WRITE Response Data:", rx_data)
-print()
+#print(" WRITE Response Data:", rx_data)
+#print()
+while True
+ # Data to be transmitted
+    tx_data = [0x03, 0x00, 0x64, 0xFF, 0xFF, 0xFF, 0xFF]
+
+# Perform SPI transaction
+    rx_data = spi.xfer2(tx_data)
+ 
+# Display received data
+    print(" READ Response Data:", rx_data)
+    time.sleep(0.2)
+
+#print()
 
 # Data to be transmitted
-tx_data = [0x03, 0x00, 0x64, 0xFF, 0xFF, 0xFF, 0xFF]
+# tx_data = [0x03, 0x00, 0x50, 0xFF, 0xFF, 0xFF, 0xFF]
 
 # Perform SPI transaction
-rx_data = spi.xfer2(tx_data)
+# rx_data = spi.xfer2(tx_data)
 
 # Display received data
-print(" READ Response Data:", rx_data)
-
-print()
-
-# Data to be transmitted
-tx_data = [0x03, 0x00, 0x50, 0xFF, 0xFF, 0xFF, 0xFF]
-
-# Perform SPI transaction
-rx_data = spi.xfer2(tx_data)
-
-# Display received data
-print("chip id Data:", rx_data)
+# print("chip id Data:", rx_data)
 
 # Close SPI
 spi.close()
