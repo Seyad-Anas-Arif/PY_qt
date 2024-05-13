ef4e723db1c3782cd55b583252c713d2c464e76a
Seyad-Anas-Arif <164306079+Seyad-Anas-Arif@users.noreply.github.com>
2024-05-13 10:48:42 +0530

    Update Basic.py

--- LAN_9252/Basic.py
+++ LAN_9252/Basic.py
@@ -4,6 +4,7 @@ import spidev
 spi = spidev.SpiDev()
 spi.open(2, 0)  # Open SPI bus 0, device 0
 spi.max_speed_hz = 30000000  # Set SPI speed to 30 MHz
+spi.mode = 0b11
 
 # Data to be transmitted
 tx_data = [0x02, 0x01, 0xF8, 0x00, 0x00, 0x00, 0x00]
@@ -15,7 +16,7 @@ print(" WRITE Received Data:", rx_data)
 print()
 
 # Data to be transmitted
-tx_data = [0x03, 0x00, 0x64, 0xFF, 0xFF, 0xFF, 0xFF]
+tx_data = [0x03, 0x00, 0x64, 0x00, 0x00, 0x00, 0x00]
 
 # Perform SPI transaction
 rx_data = spi.xfer2(tx_data)
@@ -26,7 +27,7 @@ print(" READ Received Data:", rx_data)
 print()
 
 # Data to be transmitted
-tx_data = [0x03, 0x00, 0x50, 0xFF, 0xFF, 0xFF, 0xFF]
+tx_data = [0x03, 0x00, 0x50, 0x00, 0x00, 0x00, 0x00]
 
 # Perform SPI transaction
 rx_data = spi.xfer2(tx_data)
