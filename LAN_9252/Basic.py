299fb3cb723ad63984b7a46dc973e85e34aaf683
Seyad-Anas-Arif <164306079+Seyad-Anas-Arif@users.noreply.github.com>
2024-05-13 10:18:08 +0530

    Update Basic.py

--- LAN_9252/Basic.py
+++ LAN_9252/Basic.py
@@ -9,7 +9,7 @@ spi.max_speed_hz = 30000000  # Set SPI speed to 30 MHz
 tx_data = [0x02, 0x01, 0xF8, 0x00, 0x00, 0x00, 0x00]
 
 # Perform SPI transaction
-rx_data = spi.xfer(tx_data)
+rx_data = spi.xfer2(tx_data)
 
 print(" WRITE Received Data:", rx_data)
 print()
@@ -18,7 +18,7 @@ print()
 tx_data = [0x03, 0x00, 0x64, 0xFF, 0xFF, 0xFF, 0xFF]
 
 # Perform SPI transaction
-rx_data = spi.xfer(tx_data)
+rx_data = spi.xfer2(tx_data)
 
 # Display received data
 print(" READ Received Data:", rx_data)
@@ -29,7 +29,7 @@ print()
 tx_data = [0x03, 0x00, 0x50, 0xFF, 0xFF, 0xFF, 0xFF]
 
 # Perform SPI transaction
-rx_data = spi.xfer(tx_data)
+rx_data = spi.xfer2(tx_data)
 
 # Display received data
 print("Received Data:", rx_data)
