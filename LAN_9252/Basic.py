b2426341bcc63347e8736f4cf662677a0eee8d9c
Seyad-Anas-Arif <164306079+Seyad-Anas-Arif@users.noreply.github.com>
2024-05-24 10:00:01 +0530

    Update Basic.py
    using spidev

--- LAN_9252/Basic.py
+++ LAN_9252/Basic.py
@@ -1,40 +1,45 @@
-from periphery import SPI
+import spidev
 import time
 
-spi = SPI("/dev/spidev2.0", mode=3, max_speed=3000000)  # SPI bus 2, mode 3, 5 MHz
+# Constants for SPI configuration
+SPI_BUS = 2        # SPI bus number
+SPI_DEVICE = 0     # SPI device number
+SPI_MODE = 3       # SPI mode
+SPI_SPEED = 3000000 # SPI speed in Hz (3 MHz)
 
 # Initialize SPI
-#spi = spidev.SpiDev()
-#spi.open(2, 0)  # Open SPI bus 0, device 0
-#spi.max_speed_hz = 30000000  # Set SPI speed to 30 MHz
-#spi.mode = 0b11
+spi = spidev.SpiDev()
+spi.open(SPI_BUS, SPI_DEVICE)
+spi.max_speed_hz = SPI_SPEED
+spi.mode = SPI_MODE
 
+try:
+    while True:
+        # Write operation
+        tx_data = [0x02, 0x01, 0xF8, 0x00, 0x00, 0x00, 0x00]
+        rx_data = spi.xfer(tx_data)
+        time.sleep(0.2)
+        print("WRITE Response Data:", rx_data)
+        print()
 
-while True:
- # Data to be transmitted
-    tx_data = [0x02, 0x01, 0xF8, 0x00, 0x00, 0x00, 0x00]
-# Perform SPI transaction
-    rx_data = spi.transfer(tx_data)
-    time.sleep(0.2)
-    print(" WRITE Response Data:", rx_data)
-    print()
- # Data to be transmitted
-    tx_data = [0x03, 0x00, 0x64, 0xFF, 0xFF, 0xFF, 0xFF]
-# Perform SPI transaction
-    rx_data = spi.transfer(tx_data)
-# Display received data
-    print(" READ Response Data:", rx_data)
-    time.sleep(0.2)
+        # Read operation
+        tx_data = [0x03, 0x00, 0x64, 0xFF, 0xFF, 0xFF, 0xFF]
+        rx_data = spi.xfer(tx_data)
+        print("READ Response Data:", rx_data)
+        time.sleep(0.2)
+        print()
 
-    print()
+        # Chip ID read
+        tx_data = [0x03, 0x00, 0x50, 0xFF, 0xFF, 0xFF, 0xFF]
+        rx_data = spi.xfer(tx_data)
+        print("Chip ID Data:", rx_data)
+        time.sleep(0.2)
+        print()
 
-# Data to be transmitted
-    tx_data = [0x03, 0x00, 0x50, 0xFF, 0xFF, 0xFF, 0xFF]
-# Perform SPI transaction
-    rx_data = spi.transfer(tx_data)
-# Display received data
-    print("chip id Data:", rx_data)
-    time.sleep(0.2)
+except KeyboardInterrupt:
+    # Graceful exit on Ctrl+C
+    print("Exiting...")
 
-# Close SPI
-spi.close()
+finally:
+    # Close SPI
+    spi.close()
