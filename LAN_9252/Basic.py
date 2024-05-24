bc025cb2ee4a7ea789ba199d29e8860eecbcc5f3
Seyad-Anas-Arif <164306079+Seyad-Anas-Arif@users.noreply.github.com>
2024-05-24 10:24:00 +0530

    Update Basic.py

--- LAN_9252/Basic.py
+++ LAN_9252/Basic.py
@@ -4,7 +4,7 @@ import time
 # Constants for SPI configuration
 SPI_BUS = 2         # SPI bus number (change according to your setup)
 SPI_DEVICE = 0      # SPI device number (change according to your setup)
-SPI_MODE = 1        # SPI mode
+SPI_MODE = 0        # SPI mode
 SPI_SPEED = 3000000 # SPI speed in Hz (3 MHz)
 
 # Initialize SPI
