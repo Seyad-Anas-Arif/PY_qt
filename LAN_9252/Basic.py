fbcacbc1ef5d8546793dc97ef04b6f37242e2587
Seyad-Anas-Arif <164306079+Seyad-Anas-Arif@users.noreply.github.com>
2024-05-24 10:19:35 +0530

    Update Basic.py
    spidev using error handling

--- LAN_9252/Basic.py
+++ LAN_9252/Basic.py
@@ -2,9 +2,9 @@ import spidev
 import time
 
 # Constants for SPI configuration
-SPI_BUS = 2        # SPI bus number
-SPI_DEVICE = 0     # SPI device number
-SPI_MODE = 3       # SPI mode
+SPI_BUS = 2         # SPI bus number (change according to your setup)
+SPI_DEVICE = 0      # SPI device number (change according to your setup)
+SPI_MODE = 3        # SPI mode
 SPI_SPEED = 3000000 # SPI speed in Hz (3 MHz)
 
 # Initialize SPI
@@ -16,25 +16,22 @@ spi.mode = SPI_MODE
 try:
     while True:
         # Write operation
-        tx_data = [0x02, 0x01, 0xF8, 0x00, 0x00, 0x00, 0x00]
-        rx_data = spi.xfer(tx_data)
+        tx_data_write = [0x02, 0x01, 0xF8, 0x00, 0x00, 0x00, 0x00]
+        rx_data_write = spi.xfer2(tx_data_write)
+        print("WRITE Response Data:", rx_data_write)
         time.sleep(0.2)
-        print("WRITE Response Data:", rx_data)
-        print()
 
         # Read operation
-        tx_data = [0x03, 0x00, 0x64, 0xFF, 0xFF, 0xFF, 0xFF]
-        rx_data = spi.xfer(tx_data)
-        print("READ Response Data:", rx_data)
+        tx_data_read = [0x03, 0x00, 0x64, 0xFF, 0xFF, 0xFF, 0xFF]
+        rx_data_read = spi.xfer2(tx_data_read)
+        print("READ Response Data:", rx_data_read)
         time.sleep(0.2)
-        print()
 
         # Chip ID read
-        tx_data = [0x03, 0x00, 0x50, 0xFF, 0xFF, 0xFF, 0xFF]
-        rx_data = spi.xfer(tx_data)
-        print("Chip ID Data:", rx_data)
+        tx_data_chip_id = [0x03, 0x00, 0x50, 0xFF, 0xFF, 0xFF, 0xFF]
+        rx_data_chip_id = spi.xfer2(tx_data_chip_id)
+        print("Chip ID Data:", rx_data_chip_id)
         time.sleep(0.2)
-        print()
 
 except KeyboardInterrupt:
     # Graceful exit on Ctrl+C
