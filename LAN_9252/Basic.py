85de420292a49478d999e3f39485855edfabcf0e
Seyad-Anas-Arif <164306079+Seyad-Anas-Arif@users.noreply.github.com>
2024-05-24 12:25:20 +0530

    Update Basic.py

--- LAN_9252/Basic.py
+++ LAN_9252/Basic.py
@@ -1,12 +1,11 @@
 import spidev
-import struct
 import time
 
 # Constants for SPI configuration
 SPI_BUS = 2         # SPI bus number (change according to your setup)
 SPI_DEVICE = 0      # SPI device number (change according to your setup)
 SPI_MODE = 0        # SPI mode
-SPI_SPEED = 500000 # SPI speed in Hz (5 MHz)
+SPI_SPEED = 500000  # SPI speed in Hz (500 kHz)
 
 # Initialize SPI
 spi = spidev.SpiDev()
@@ -14,26 +13,26 @@ spi.open(SPI_BUS, SPI_DEVICE)
 spi.max_speed_hz = SPI_SPEED
 spi.mode = SPI_MODE
 
+def int_to_byte_list(value, length):
+    return [(value >> (8 * i)) & 0xFF for i in range(length-1, -1, -1)]
+
 try:
     while True:
         # Write operation
-        tx_data_write = 565114616938496
-        data_out = struct.pack('B', tx_data_write)
-        rx_data_write = spi.xfer2([data_out])
+        tx_data_write = int_to_byte_list(0x0201F800000000, 7)
+        rx_data_write = spi.xfer2(tx_data_write)
         print("WRITE Response Data:", rx_data_write)
         time.sleep(0.2)
 
         # Read operation
-        tx_data_read = 844858721828863
-        data_out = struct.pack('B', tx_data_read)
-        rx_data_read = spi.xfer2([data_out])
+        tx_data_read = int_to_byte_list(0x030064FFFFFFFF, 7)
+        rx_data_read = spi.xfer2(tx_data_read)
         print("READ Response Data:", rx_data_read)
         time.sleep(0.2)
 
         # Chip ID read
-        tx_data_chip_id = 844858721828863
-        data_out = struct.pack('B', tx_data_chip_id)
-        rx_data_chip_id = spi.xfer2([data_out])
+        tx_data_chip_id = int_to_byte_list(0x030050FFFFFFFF, 7)
+        rx_data_chip_id = spi.xfer2(tx_data_chip_id)
         print("Chip ID Data:", rx_data_chip_id)
         time.sleep(0.2)
 
