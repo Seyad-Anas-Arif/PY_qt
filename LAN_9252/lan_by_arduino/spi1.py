4c9f9aacd3e5226b5ca82296ad882bed07cec1d6
Seyad-Anas-Arif <164306079+Seyad-Anas-Arif@users.noreply.github.com>
2024-05-27 12:00:28 +0530

    Update spi1.py

--- LAN_9252/lan_by_arduino/spi1.py
+++ LAN_9252/lan_by_arduino/spi1.py
@@ -3,7 +3,7 @@ import spidev
 from lanh import *
 spi = spidev.SpiDev() 
 def LAN925X_SPI_Init():
-    spi = spidev.SpiDev()
+  #  spi = spidev.SpiDev()
     spi.open(2, 0)  # Use SPI bus 0, device 0
     spi.max_speed_hz = 3000000  # Set SPI speed to 3 MHz
     spi.mode = 0b11  # Set SPI mode to 0b00 (CPOL=0, CPHA=0)
@@ -38,5 +38,5 @@ def LAN925X_SPI_WRITE( addr, data):
     SPI_8bit_Write( (data >> 16) & 0xFF)  # Data byte 2
     SPI_8bit_Write( (data >> 24) & 0xFF)  # Data byte 3
 
-def spi_close():
+def LAN9252_CLOSE():
      spi.close()
